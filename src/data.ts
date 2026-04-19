7031a9a9aabc357ee12043d24a7b17d5d9b1b67e
montmorill <montmorill@outlook.com>
2026-04-20 02:44:31 +0800

    Refactor lexicon data structure and alias generation
    Updates the makeAlias function to accept arrays instead of dictionaries for clarity
    Reorders some lexicon entries for better organization and adds explicit "仄" category
    Improves comments to clarify the purpose of generated alias mappings

--- src/data.ts
+++ src/data.ts
@@ -6,8 +6,8 @@ import 百家姓 from './data/百家姓.json'
 import standard from './data/通用规范汉字表.json'
 import { Lexicon } from './lexicon'
 
-function makeAlias(dict: Record<string, any>, f: (key: string) => string) {
-  return Object.fromEntries(Object.keys(dict).map(key => [f(key), key]))
+function makeAlias(array: string[], f: (key: string) => string) {
+  return Object.fromEntries(array.map(key => [f(key), key]))
 }
 
 type Yunbu = keyof UnionToIntersection<typeof pingshui[keyof typeof pingshui]>
@@ -18,14 +18,15 @@ export default new Lexicon({
   平水韵: Object.keys(yunbus),
   ...yunbus, // 一东，二冬，三江，四支……
   ...standard,
-  天干: '甲乙丙丁戊己庚辛壬癸',
-  地支: '子丑寅卯辰巳午未申酉戌亥',
   百家姓,
   千字文,
+  天干: '甲乙丙丁戊己庚辛壬癸',
+  地支: '子丑寅卯辰巳午未申酉戌亥',
 }, {
-  ...mapValues(pingshui, dict => Object.keys(dict).join('+')),
-  ...makeAlias(yunbus, key => key[key.length - 1]),
-  ...makeAlias(standard, key => key[0]),
+  仄: '上+去+入',
+  ...mapValues(pingshui, dict => Object.keys(dict).join('+')), // 平(声)，上(声)，去(声)，入(声)
+  ...makeAlias(Object.keys(yunbus), key => key[key.length - 1]), // (一)东，(二)冬，(三)江，(四)支……
+  ...makeAlias(Object.keys(standard), key => key[0]), // 一(级字)，二(级字)，三(级字)
   通用规范汉字: Object.keys(standard).join('+'),
   通规: '通用规范汉字',
 })
